ad0fed98f785b0a28390cc48a4bff68697aeb41d
K L Latha <lathakljigali@gmail.com>
2024-01-11 21:03:26 +0530

    first commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh'],
+  globals: 
+  { "angular": false, 
+  "module": false, 
+  "inject": false, 
+  "document": false },
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExports: true },
+    ],
+  },
+}
